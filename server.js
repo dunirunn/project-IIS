ce387c736b1f4c5df340c11048286fdcdce2fc13
dunirunn <dunaevaadelya@gmail.com>
2026-04-29 17:36:50 +0500

    z

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ const port = process.env.PORT || 5000;
 
 // Сообщение о том, что сервер запущен и прослушивает указанный порт 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+}); 
