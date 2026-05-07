cc4db75e2ac906ed63e01c5c93b3e2e1655a3aa0
Xeon_Fox <78652129+aerosness@users.noreply.github.com>
2026-05-07 21:14:17 +0500

    a

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
         <NavLink to={ROUTES.HOME} className="logo-link" aria-label="Home">
           <img src={LOGO_IMG_SRC} alt="ComicNest logo" className="logo-img" />
           <span className="logo-text">
-            <span className="bracket">[</span>ComicNest<span className="bracket">]</span>
+            <span className="bracket">[</span>Comic Roots<span className="bracket">]</span>
           </span>
         </NavLink>
 
